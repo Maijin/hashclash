0a2207f9b51ed41ed6cdc0fa3f3b0dfa8b3b43ee
Maijin <maijin@reverseshade.com>
2019-10-23 14:47:11 +0200

    Fix build.sh for Mac OS

--- build.sh
+++ build.sh
@@ -1,54 +1,44 @@
-#!/bin/bash
-
-# default values for boost library version and install location
-# (if not already defined in environment)
+#!/bin/sh
 
 : ${BOOST_VERSION:=1.57.0}
 : ${BOOST_INSTALL_PREFIX:=$(pwd)/boost-$BOOST_VERSION}
-: ${INCLUDE_DIRS:="/usr/include /usr/local/include"}
-
-function check_for_tool
-{
-	echo -n "[*] checking for system tool: $1: "
-	if [ -z $(command -v $2) ]; then
-		echo "*** error missing ***"
-		exit 1
-	fi
-	echo "found"
-}
-
-function check_for_library
-{
-	echo -n "[*] checking for system library: $1: "
-	have_lib=no
-	for d in ${INCLUDE_DIRS}; do
-		if [ -f $d/$2 ]; then
-			have_lib=yes
-			break
-		fi
-	done
-	if [ "${have_lib}" = "no" ]; then
-		echo "*** error missing ***"
-		exit 1
-	fi
-	echo "found"
-}
 
 ## Check prerequisites
 
-check_for_tool autoconf autoreconf
-check_for_tool automake automake
-check_for_tool libtool libtoolize
+echo -n "[*] Checking for system packages: autoconf, automake & libtool: "
 
-check_for_library zlib1g-dev zlib.h
-check_for_library libbz2-dev bzlib.h
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	SDK_PATH=$(xcrun --sdk macosx --show-sdk-path)
+fi
 
-# Check for local boost libraries and compile it if necessary
+if glibtoolize --version >/dev/null 2>&1; then
+	LIBTOOLIZE='glibtoolize'
+else
+	LIBTOOLIZE='libtoolize'
+fi
+
+if [ -z $(command -v autoreconf) ] || [ -z $(command -v automake) ] || [ -z $(command -v $LIBTOOLIZE) ]; then
+	echo "*** error missing ***"
+	exit 1
+fi
+echo "found"
+
+echo -n "[*] Checking for libraries: zlib and bz2: "
+have_zlib=no
+have_bz2=no
+for d in /usr/include /usr/local/include $SDK_PATH/usr/include/; do
+	if [ -f $d/zlib.h ]; then have_zlib=yes; fi
+	if [ -f $d/bzlib.h ]; then have_bz2=yes; fi
+done
+if [ "${have_zlib}" = "no" ] || [ "${have_bz2}" = "no" ]; then
+	echo "*** error missing ***"
+	exit 1
+fi
+echo "found"
 
 echo -n "[*] Checking for local boost (version ${BOOST_VERSION}): "
 if [ ! -f ${BOOST_INSTALL_PREFIX}/include/boost/version.hpp ]; then
 	echo "need to compile"
-	echo "[*] Run: ./install_boost.sh"
 	export BOOST_VERSION
 	export BOOST_INSTALL_PREFIX
 	./install_boost.sh || exit 1
@@ -56,12 +46,8 @@ else
 	echo "found"
 fi
 if [ ! -f ${BOOST_INSTALL_PREFIX}/include/boost/version.hpp ]; then
-	echo "[*] *** error *** cannot find local boost (version ${BOOST_VERSION})"
-	exit 1
+	echo "[*] Cannot find local boost (version ${BOOST_VERSION})"
 fi
-
-
-# Compile HashClash
 
 echo "[*] Run: autoreconf --install"
 autoreconf --install || exit 1
